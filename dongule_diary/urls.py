ca329f10e4df7474b8d09b6b5e5bb282e8b18745
ssa <wogur981208@gmail.com>
2022-12-28 17:25:59 +0900

    :tada:Chore: 프로젝트 빌드
    -1. 프로젝트 빌드를 했습니다
    -2. swagger, corsheader, rest_framework를 적용했습니다.
    -3. pip 패키지 빌드했습니다. (black)

---
+++ dongule_diary/urls.py
@@ -0,0 +1,35 @@
+from rest_framework import permissions
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="동글동글 다이어리",
+        default_version='v1',
+        description="동글동글 다이어리 API",
+        terms_of_service="https://www.ourapp.com/policies/terms/",
+        contact=openapi.Contact(email="wogur981208@gmail.com"),
+        license=openapi.License(name="DonguleDongule Diary License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    # Admin
+    path('admin/', admin.site.urls),
+    
+    # Apps
+    
+    # Swagger
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/api.json/', schema_view.without_ui(cache_timeout=0), name='schema-swagger-json'), 
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
