d4b6fe57d23690eccb0f042ecbc16d13a0dbdb2a
marinred <endrms1995@gmail.com>
2023-01-15 14:40:20 +0900

    :sparkles:Feat: 회원가입, 로그인, 로그아웃 url 설정 #2
    회원가입, 로그인, 로그아웃 url 설정

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from users import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name = "users"
 
 urlpatterns = [
+    path('signup/', views.UserView.as_view(), name='user_view'),
+    path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/blacklist/', views.UserLogoutViews.as_view(), name='logout'),
 ]
