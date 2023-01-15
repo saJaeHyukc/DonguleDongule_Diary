943db3fdb2359b99a99929d0b472f3fdf76ab370
marinred <endrms1995@gmail.com>
2023-01-15 14:45:36 +0900

    :sparkles:Feat: 회원가입, 로그인, 로그아웃 views.py
    회원가입, 로그인, 로그아웃 views.py 기능 추가

--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
+from users.models import User
+from django.shortcuts import get_object_or_404
+from users.serializers import UserSerializer
+from users.serializers import CustomObtainPairSerializer
+from users.serializers import LogoutSerializer
 
 # Create your views here.
+class UserView(APIView):
+    # signup
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "회원가입 완료"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({"message": f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+        
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomObtainPairSerializer
+    
+class UserLogoutViews(APIView):
+    def post(self, request):
+        serializer = LogoutSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "로그아웃 완료"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
